f97db7f5dc897fb672400e17bf2bd72a0a5dcbe8
Job Ya Mkenya <jobyamkenya@gmail.com>
2018-04-15 21:56:15 +0300

    if else task statement

---
+++ ifelse/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let jsNames = prompt('what is the official names of javascript?', '');
+
+if(jsNames == "ECMAScript") {
+	alert("RIght!");
+} else {
+	alert("Didn’t know? ECMAScript!");
+}
