88fe7ab195c1967494b1e418216ffedb6372c6ef
Job Ya Mkenya <jobyamkenya@gmail.com>
2018-04-15 22:49:36 +0300

    tenary complete

---
+++ ternaryOperation/main.js
@@ -0,0 +1,13 @@
+'use strict'
+/*convert if statement this to ternary
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+*/
+let a=3;
+let b=2;
+let sum = (a + b < 7) ? 'Below' : 'Over';
+alert(sum);
